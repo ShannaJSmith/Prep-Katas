8190ece487a33bf9a3e53e1ae799dece1a527aae
Shanna J. Smith <shannasmith@Shannas-Air.phub.net.cable.rogers.com>
2021-12-08 15:27:23 -0500

    add console.logs

--- kata2-conditionalSums.js
+++ kata2-conditionalSums.js
@@ -14,3 +14,8 @@ const conditionalSum = function(values, condition) {
   }
   return num;
 };
+
+console.log(conditionalSum([1, 2, 3, 4, 5], "even"));
+console.log(conditionalSum([1, 2, 3, 4, 5], "odd"));
+console.log(conditionalSum([13, 88, 12, 44, 99], "even"));
+console.log(conditionalSum([], "odd"));
