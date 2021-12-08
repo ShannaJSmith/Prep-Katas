be3d2511691fe984b19f479c71ef0960388c7785
Shanna J. Smith <shannasmith@Shannas-Air.phub.net.cable.rogers.com>
2021-12-08 17:43:11 -0500

    kata13

---
+++ kata13-talkingCalender.js
@@ -0,0 +1,12 @@
+const talkingCalendar = function(date) {
+  // Your code here
+};
+
+console.log(talkingCalendar("2017/12/02"));
+console.log(talkingCalendar("2007/11/11"));
+console.log(talkingCalendar("1987/08/24"));
+
+// EXPECTED OUTPUT:
+// December 2nd, 2017
+// November 11th, 2007
+// August 24th, 1987
