8d5c043c68ee1f7e0cfd98b976e8e9da1a0f5274
Shanna J. Smith <shannasmith@Shannas-Air.phub.net.cable.rogers.com>
2021-12-09 13:32:08 -0500

    work so far for kata14

--- kata14.js
+++ kata14.js
@@ -1,5 +1,13 @@
 const calculateChange = function(total, cash) {
-  // Your code here
+  let change = cash - total;
+  let currencyDenominations = ['twentyDollar', 'tenDollar', 'fiveDollar', 'twoDollar', 'oneDollar', 'quarter', 'dime', 'nickle', 'penny'];
+  let values = [2000, 1000, 500, 200, 100, 25, 10, 5, 1];
+  let changeObj = {};
+
+  for (let i = 0; i < values.length; i++) {
+    console.log("value", values[i])
+  }
+  // return changeObj;
 };
 
 console.log(calculateChange(1787, 2000));
