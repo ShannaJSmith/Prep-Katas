1c3c5c3d2f46985ae7fb4110f801e139609dc0a7
Shanna J. Smith <shanna_smith987@hotmail.com>
2021-12-10 14:01:41 -0500

    kata16 complete camel, pascal and snakecase

--- kata16.js
+++ kata16.js
@@ -1,15 +1,77 @@
-const makeCase = function(input, caseType) {
-  // Put your solution here
+// const camelCase = (input) => {
+//   let array = input.split(" ");
+//   let answer = array.map((word, index) => {
+//     if (index === 0) {
+//       return word.toLowerCase();
+//     }
+//     return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+//   })
+//   return answer.join("");
+// }
+
+// console.log(camelCase('this is a string'))
+
+// const pascalCase = (input) => {
+//   let array = input.split(" ");
+//   let answer = array.map ((word, index) => {
+//     return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+//   })
+//   return answer.join("");
+// }
+
+// console.log(pascalCase('this is a string'))
+
+const snakeCase = (input) => {
+  let array = input.split(" ")
+  return array.join("_")
 }
 
-console.log(makeCase("this is a string", "camel"));
-console.log(makeCase("this is a string", "pascal"));
-console.log(makeCase("this is a string", "snake"));
-console.log(makeCase("this is a string", "kebab"));
-console.log(makeCase("this is a string", "title"));
-console.log(makeCase("this is a string", "vowel"));
-console.log(makeCase("this is a string", "consonant"));
-console.log(makeCase("this is a string", ["upper", "snake"]));
+console.log(snakeCase('this is a string'))
+
+// const kebabCase = (input) => {
+  
+// }
+
+// const titleCase = (input) => {
+  
+// }
+
+// const vowelCase = (input) => {
+  
+// }
+
+// const consonantCase = (input) => {
+  
+// }
+
+// const upperCase = (input) => {
+  
+// }
+
+// const lowerCase = (input) => {
+  
+// }
+
+// const makeCase = function (input, caseType) {
+//   let array = input.split(' ');
+//     if (caseType === 'camel') { 
+//     } else if (caseType === 'pascal') {
+//     } else if (caseType === 'snake') {
+//     } else if (caseType === 'kebab') {
+//     } else if (caseType === 'title') {
+//     } else if (caseType === 'vowel') {
+//     } else if (caseType === 'consonant') {
+//     } else caseType === 'upper' && caseType === 'snake'
+// }
+
+// console.log(makeCase('this is a string', 'camel'))
+// console.log(makeCase('this is a string', 'pascal'))
+// console.log(makeCase('this is a string', 'snake'))
+// console.log(makeCase('this is a string', 'kebab'))
+// console.log(makeCase('this is a string', 'title'))
+// console.log(makeCase('this is a string', 'vowel'))
+// console.log(makeCase('this is a string', 'consonant'))
+// console.log(makeCase('this is a string', ['upper', 'snake']))
 
 // Expected Output:
 // thisIsAString
