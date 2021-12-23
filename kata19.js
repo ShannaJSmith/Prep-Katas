5e226f789a51ad5608fd25f1c0a6c3c9594a5067
Shanna J. Smith <shanna_smith987@hotmail.com>
2021-12-22 23:35:15 -0500

    fix syntax and formatting. extra element now fixed

--- kata19.js
+++ kata19.js
@@ -3,35 +3,44 @@
 // Create a function generateBoard which will return a nested array representing the board, containing the location of two queens.
 // Create a function called queenThreat that will indicate whether or not the two queens are positioned so that they attack each other.
 
-let whiteQueen = [0, 5];
-let blackQueen = [5, 0];
+let whiteQueen = [0, 5]
+let blackQueen = [5, 0]
 
 // will return game board containing location of 2 queens
 const generateBoard = (wQueen, bQueen) => {
-let gameBoard = [];
-for (let i = 0; i < 8; i++) {
-  gameBoard.push([]);
-}
+  let gameBoard = []
 
-for (let i = 0; i < 8; i++) {
-  for (let j = 0; j < 8; j++) {
-    // console.log("wq", wQueen[0])
-    // console.log("wq1", wQueen[1])
-    if (i === wQueen[0] && j === wQueen[1]) {
-      gameBoard[i].push(1);
-    } 
-      gameBoard[i].push(0);
+  for (let i = 0; i < 8; i++) {
+    // create 8 length empty array to start
+    gameBoard.push([])
+  }
+  // console.log("gameboard", gameBoard)
+
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      // console.log("wq", wQueen[0])
+      // console.log("wq1", wQueen[1])
+      if (
+        (i === wQueen[0] && j === wQueen[1]) ||
+        (i === bQueen[0] && j === bQueen[1])
+      ) {
+        // if the inputted coordinates match the index then print a 1 for an occupied space
+        gameBoard[i].push(1)
+      } else {
+        // if not print a 0 for an unoccupied space
+        gameBoard[i].push(0)
+      }
     }
   }
-return gameBoard;
+  return gameBoard
 }
 
 // const queenThreat = () => {
 // // will return true or false
 // }
 
-let generatedBoard = generateBoard(whiteQueen, blackQueen);
-console.log(generatedBoard);
+let generatedBoard = generateBoard(whiteQueen, blackQueen)
+console.log(generatedBoard)
 // console.log(queenThreat(generatedBoard));
 
 // Expected Output
