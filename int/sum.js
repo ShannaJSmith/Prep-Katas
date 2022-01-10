e9da3606577bcfc9daee1402de95a1267f395ca4
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-01-10 14:06:43 -0500

    edit sum.js

--- int/sum.js
+++ int/sum.js
@@ -1,6 +1,6 @@
 // determine if each number is greater than the sum of all prev elements in an array
 
-function ArrayChallenge(arr) { 
+const arrayChallenge = (arr) => { 
   let sum = 0;
   for (let i = 0; i < arr.length - 1; i++) {
     sum += arr[i];
@@ -11,4 +11,4 @@ function ArrayChallenge(arr) {
   return "true"; 
 }
 
-console.log(sum([1, 2, 3, 4, 5]));
+console.log(arrayChallenge([1, 2, 3, 4, 5]));
