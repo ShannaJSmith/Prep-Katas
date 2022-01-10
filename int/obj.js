53c958677a9628010e893f9c8bac91d47c5d13d0
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-01-09 23:52:41 -0500

    add another exercise in tally.js and obj.js

---
+++ int/obj.js
@@ -0,0 +1,35 @@
+const obj = {
+  name: { firstname: 'bob', middle: '', lastname: 'miller' },
+  age: 25,
+  dob: '-',
+  hobbies: ['running', 'wathing anime', '-'],
+  education: { highschool: 'mss', college: 'N/A' },
+}
+
+const mod = (obj) => {
+  // console.log("obj", obj)
+  Object.entries(obj).forEach(([key, value]) => {
+    if (value && typeof value === 'object') {
+      mod(value)
+    }
+    if ((value && typeof value === 'object' && !Object.keys(value).length) ||
+      value === 'N/A' ||
+      value === '-' ||
+      value === ''
+    ) {
+      if (Array.isArray(obj)) {
+        obj.splice(key, 1)
+      } else {
+        delete obj[key]
+      }
+    }
+  })
+  return obj
+}
+
+console.log("answer", mod(obj))
+
+// console.log("obj:", obj.name.firstname)
+// console.log("keys:", Object.keys(obj))
+// console.log("values:", Object.values(obj))
+// console.log("entires:", Object.entries(obj))
