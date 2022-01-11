7ad049cdf9f836cec12e59ba989a1c1c3179e0a0
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-01-11 17:29:44 -0500

    implement negative number condition

--- int/paper.js
+++ int/paper.js
@@ -1,12 +1,19 @@
 /*
-  Given that a piece of paper is 0.0001m thick, Create a function that takes the argument of distance and calculate how many times the paper must be folded in order to reach that distance. If the distance is negative, return null
-
+  Given that a piece of paper is 0.0001m thick, 
+  Create a function that takes the argument of distance and calculate how many times the paper must be folded in order to reach that distance. 
+  If the distance is negative, return null.
   example:
   input: 1000m
   output: 1
 */
 
-function calculateFolds(distance){
+const calculateFolds = (distance) => {
+  if (distance < 0) {
+    return null
+  }
+  return true
 }
+
+console.log(calculateFolds(1000))
 
 module.exports = calculateFolds
