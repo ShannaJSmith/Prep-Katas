9f7b4e6952040ddac0492096c9a184df020b03b3
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-01-11 17:43:28 -0500

    declare thickness

--- int/paper.js
+++ int/paper.js
@@ -1,6 +1,6 @@
 /*
   Given that a piece of paper is 0.0001m thick, 
-  Create a function that takes the argument of distance and calculate how many times the paper must be folded in order to reach that distance. 
+  create a function that takes the argument of distance and calculate how many times the paper must be folded in order to reach that distance. 
   If the distance is negative, return null.
   example:
   input: 1000m
@@ -8,10 +8,11 @@
 */
 
 const calculateFolds = (distance) => {
+  let thickness = 0.0001;
+  let fold = thickness * 2;
   if (distance < 0) {
     return null
   }
-  return true
 }
 
 console.log(calculateFolds(1000))
