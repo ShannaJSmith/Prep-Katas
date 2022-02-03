75af399492bec3c9b15203060ed4ef2bfae52054
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-02-03 11:14:24 -0500

    add question to obj.js

--- int/obj.js
+++ int/obj.js
@@ -1,39 +1,41 @@
+// tech question: do an http/axios/etc call to retrieve an object. Clean the object so that all n/a, '-' and "" fields are removed
+
 const obj = {
   name: { firstname: 'bob', middle: '', lastname: 'miller' },
   age: 25,
   dob: '-',
   hobbies: ['running', 'wathing anime', '-'],
   education: { highschool: 'mss', college: 'N/A' },
-}
+};
 
 const mod = (obj) => {
   // console.log("obj", obj)
   Object.entries(obj).forEach(([key, value]) => {
     if (value && typeof value === 'object') {
-      mod(value)
+      mod(value);
     }
-    if ((value && typeof value === 'object' && !Object.keys(value).length) ||
+    if (
+      (value && typeof value === 'object' && !Object.keys(value).length) ||
       value === 'N/A' ||
       value === '-' ||
       value === ''
     ) {
       if (Array.isArray(obj)) {
-        obj.splice(key, 1)
+        obj.splice(key, 1);
       } else {
-        delete obj[key]
+        delete obj[key];
       }
     }
-  })
-  return obj
-}
+  });
+  return obj;
+};
 
-console.log("answer", mod(obj))
+console.log('answer', mod(obj));
 
 // console.log("obj:", obj.name.firstname)
 // console.log("keys:", Object.keys(obj))
 // console.log("values:", Object.values(obj))
 // console.log("entires:", Object.entries(obj))
-
 
 // const axios = require('axios');
 
